833171d8a7b6dc7b781d85eda00f73429725eb45
aayushbatwara <42208400+aayushbatwara@users.noreply.github.com>
2024-10-27 17:01:09 +0800

    Update quotes.ts

--- src/quotes.ts
+++ src/quotes.ts
@@ -1,6 +1,5 @@
 export const quotes = [
   "Your manager is cash, your mentor is credit, and your networking is an investment",
   "Effort that is not recognized is effort that is not done",
-  "So long Hong Kong, catch me for a tea in London with my subtle British accent!",
-  "Testing"
+  "So long Hong Kong, catch me for a tea in London with my subtle British accent!"
 ];
